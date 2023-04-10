ae34e5059e66e592cc56686fa147d535236d1c36
deepthi-vikram <Deepthi.Vikram@elevancehealth.com>
2023-04-10 16:59:19 -0400

    repo changes

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/kontent-angular-sample'),
+      dir: require('path').join(__dirname, './coverage/kontent-angular-poc'),
       subdir: '.',
       reporters: [
         { type: 'html' },
